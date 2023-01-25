c7533db703eb5f11d3e8436efbf52ef3bb912fa0
snd <viper.ghost@bk.ru>
2023-01-26 00:06:53 +0300

    add Profile, Skill models. Connect each other

--- app_users/models.py
+++ app_users/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    fullname = models.CharField(max_length=200)
+    short_intro = models.CharField(max_length=200)
+    bio = models.TextField(null=True, blank=True)
+    photo = models.ImageField(default='user-default.png', upload_to='profiles/', null=True, blank=True)
+    skills = models.ManyToManyField(to='Skill')
+
+    social_twitter = models.URLField(max_length=200, null=True, blank=True)
+    social_instagram = models.URLField(max_length=200, null=True, blank=True)
+    social_facebook = models.URLField(max_length=200, null=True, blank=True)
+    social_linkedin = models.URLField(max_length=200, null=True, blank=True)
+    social_telegram = models.URLField(max_length=200, null=True, blank=True)
+    social_stackoverflow = models.URLField(max_length=200, null=True, blank=True)
+    social_github = models.URLField(max_length=200, null=True, blank=True)
+    social_website = models.URLField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.fullname}'
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
