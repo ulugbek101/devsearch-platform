a13a2f95e82194999f94b64a57925eaebbafdbae
Ulugbek Umaraliyev <94630185+ulugbek101@users.noreply.github.com>
2023-06-19 17:16:06 +0500

    Add 'devsearch.com' domain name to allowed hosts

--- DEVSEARCH/settings.py
+++ DEVSEARCH/settings.py
@@ -19,10 +19,11 @@ SECRET_KEY = env.str('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.str('DEBUG', False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'dev-search.up.railway.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'dev-search.up.railway.app', 'dev-search.com']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://dev-search.up.railway.app'
+    'https://dev-search.up.railway.app',
+    'https://dev-search.com',
 ]
 
 # Application definition
